f9f542ab33d66897e046f1cc7376023e185d38cf
ninjacop1 <wilhelm.andersen98@gmail.com>
2024-10-17 10:36:39 +0200

    Update CTRL.vhd
    test

--- CTRL.vhd
+++ CTRL.vhd
@@ -1,1 +1,21 @@
 entity
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.STD_LOGIC_ARITH.ALL;
+
+  entity CTRL is
+    port
+      (
+        
+clk: in std logicvect
+rst :in std_logic_vector
+snd :in std_logic_vector
+snd_led :out std_logic_vector
+
+tx_data : out
+tx_ready:
+rx_data
+rx_ready
+
+baud_select
+parity_select
